b39bc16f1df84c982c65ad4bc88f47b6539a5efb
Trevor Grandle <trevor.grandle@ge.com>
2015-04-27 13:33:59 -0400

    code looks correct

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
+/*global ctx, Resources */
+
 /* this file contains the app logic
 * and has been updated by Trevor Grandle
 * for the Udacity nanodegree P3
 */
 
+// global constants
+var XSIZE = 101;
+var YSIZE = 83;
+var XMIN = 0;
+var XMAX = 5 * this.XSIZE;
+var YMIN = -25;
+var YMAX = (6 * this.YSIZE) - 25;  
+
+
 // Enemies our player must avoid
 var Enemy = function () {
+  'use strict';
 
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
@@ -14,8 +26,11 @@ var Enemy = function () {
   this.sprite = 'images/enemy-bug.png';
 
   var speed;
-}
+};
+
 Enemy.prototype.init = function () {
+  'use strict';
+
   //random number between 1 and 100
   this.speed = Math.floor((Math.random() * 100) + 1);
   console.log('enemy speed is ' + this.speed);
@@ -28,11 +43,14 @@ Enemy.prototype.init = function () {
   this.y = newX * YSIZE - 25;
   console.log('enemy y is ' + this.y);
   console.log('enemy x is ' + this.x);
-  console.log(YSIZE + " : " + XSIZE);
-}
+  console.log(YSIZE + ' : ' + XSIZE);
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+  'use strict';
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -47,98 +65,91 @@ Enemy.prototype.update = function(dt) {
       this.x = -101;
       this.init();
     }    
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+  'use strict';
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(){
-    this.sprite = 'images/char-boy.png';
-  this.move = "";
+  'use strict';
 
-}
+    this.sprite = 'images/char-boy.png';
+  this.move = '';
+
+};
+
 Player.prototype.init = function(){
+  'use strict';
+
     //start in the middle of a tile
-  this.x = 2 * XSIZE
+  this.x = 2 * XSIZE;
   this.y = -25 + (5 * YSIZE);    
 
   console.log('player y is ' + this.y);
   console.log('player x is ' + this.x);
-  console.log(YSIZE + " : " + XSIZE);
-}
+  console.log(YSIZE + ' : ' + XSIZE);
+};
+
 Player.prototype.update = function(){
+  'use strict';
+
   //only move if we got a key press
-  if(this.move !== ""){
-    if(this.move === "left"){
+  if(this.move !== ''){
+    if(this.move === 'left'){
         this.x += -XSIZE;
-        this.move = "";
-    }if(this.move == "right"){
+        this.move = '';
+    }if(this.move === 'right'){
         this.x += XSIZE;
-        this.move = "";
-    }if(this.move == "up"){
+        this.move = '';
+    }if(this.move === 'up'){
         this.y += -YSIZE;
-        this.move = "";
-    }if(this.move == "down"){
+        this.move = '';
+    }if(this.move === 'down'){
         this.y += YSIZE;
-        this.move = "";
+        this.move = '';
     }
 
     //stop moving off the board
     if(this.x < XMIN){
       this.x = XMIN;
-      console.log("bounding x to " + this.x);
+      console.log('bounding x to ' + this.x);
     }
     if(this.x >= XMAX){
       this.x = XMAX - XSIZE;
-      console.log("bounding x to " + this.x);
+      console.log('bounding x to ' + this.x);
     }
     if(this.y < YMIN){
       this.y = YMIN;
-      console.log("bounding y to " + this.y);
+      console.log('bounding y to ' + this.y);
     }
     if(this.y >= YMAX){
       this.y = YMAX - YSIZE;
-      console.log("bounding y to " + this.y);
+      console.log('bounding y to ' + this.y);
     }
   }
 
-    //check for collision
-    /*
-    allEnemies.forEach(function(enemy) {
-        var xOverlap = Math.abs(enemy.x - player.x);
-        var yOverlap = Math.abs(enemy.y - player.y);
+};
 
-        if((xOverlap < XSIZE) && (yOverlap < YSIZE)){
-            console.log('!!!!!!! you died !!!!!!!');
-            
-        }
-    }); 
-    */ 
-
-}
 Player.prototype.render = function() {
+  'use strict';
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 Player.prototype.handleInput = function(key) {
+  'use strict';
+
     console.log(key);
     this.move = key;
 
-}
-
-
-// global constants
-var XSIZE = 101;
-var YSIZE = 83;
-var XMIN = 0;
-var XMAX = 5 * this.XSIZE;
-var YMIN = -25;
-var YMAX = (6 * this.YSIZE) - 25;  
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -153,11 +164,11 @@ var allEnemies = [e1, e2, e3];
 var player = new Player();
 player.init();
 
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
+  'use strict';
+
     var allowedKeys = {
         37: 'left',
         38: 'up',
